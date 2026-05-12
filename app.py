30509d4c567d2828b238d2ec1cf6f1e0307a275a
Gerald McAuley <geraldmcauley@gmail.com>
2026-05-12 17:13:36 +0100

    remove uneeded import

---
+++ app.py
@@ -0,0 +1,2 @@
+from flask import Flask, request, render_template # This imports Flask for creating the web application, request for reading incoming HTTP data, and render_template for rendering an HTML template with variables.
+from PIL import Image # Pill is a common library for loading and manipulating images
